7dba1d11072f94f7804f1faf419af382ec456470
uonick <uonick@users.noreply.github.com>
2017-01-10 22:46:48 +0300

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -121,7 +121,7 @@ sudo service nginx restart > /dev/null
 #
 echo -e "----------------------------------------"
 echo "VAGRANT ==> PHP 7"
-sudo apt-get install -y php7.0-fpm php7.0-cli php7.0-common php7.0-json php7.0-opcache php7.0-mysql php7.0-phpdbg php7.0-mbstring php7.0-gd php-imagick  php7.0-pgsql php7.0-pspell php7.0-recode php7.0-tidy php7.0-dev php7.0-intl php7.0-gd php7.0-curl php7.0-zip php7.0-xml mcrypt memcached phpunit
+sudo apt-get install -y php7.0-fpm php7.0-cli php7.0-common php7.0-json php7.0-opcache php7.0-mysql php7.0-phpdbg php7.0-mbstring php7.0-gd php-imagick  php7.0-pgsql php7.0-pspell php7.0-recode php7.0-tidy php7.0-dev php7.0-intl php7.0-gd php7.0-curl php7.0-zip php7.0-xml php-memcached mcrypt memcached phpunit
 
 
 #
